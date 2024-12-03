2b73dfccb26ac18a774edeaec6854c3e4a77ef51
Manuel Morteo <manuelmort@dhcp-acadmin-204-8-153-28.bu.edu>
2024-12-03 17:58:45 -0500

    Fixed a bug

--- MiscSimulation/jacobian.py
+++ MiscSimulation/jacobian.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from TwoLinkInverse import inverse_kinematics_2link_elbow_up
+from TwoLink.TwoLinkInverse import inverse_kinematics_2link_elbow_up
 
 a1 = 5
 a2 = 4
